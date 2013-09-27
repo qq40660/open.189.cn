952f081b84f4dad551e2ebc1fe5ccd09a04ffa14
wayne <wayne@chen.pro>
2013-09-27 11:33:27 +0800

    add sendSMS api support

--- Python/RandCode/RandCode.py
+++ Python/RandCode/RandCode.py
@@ -14,6 +14,7 @@ class RandCode(object):
 	TOKEN_API = 'https://oauth.api.189.cn/emp/oauth2/v2/access_token'
 	RANDCODE_TOKEN_API = 'http://api.189.cn/v2/dm/randcode/token'
 	RANDCODE_SEND_API = 'http://api.189.cn/v2/dm/randcode/send'
+	RANDCODE_SENDSMS_API = 'http://api.189.cn/v2/dm/randcode/sendSms'
 
 	
 	def __init__(self, app_id='', app_secret='', access_token=''):
@@ -22,14 +23,14 @@ class RandCode(object):
 		self.ACCESS_TOKEN = access_token or self.__fetch_access_token()
 		self.RANDCODE_TOKEN = self.__fetch_randcode_token()
 
-	def send(self, mobile, url, exp_time):
+	def send(self, phone, url, exp_time):
 		result = False
 		if self.ACCESS_TOKEN and self.RANDCODE_TOKEN:
 			data = {
 				'app_id':self.APP_ID,
 				'access_token':self.ACCESS_TOKEN,
 				'token':self.RANDCODE_TOKEN,
-				'phone':mobile,
+				'phone':phone,
 				'url':url,
 				'exp_time':exp_time,
 				'timestamp':self.__date_time(),
@@ -42,6 +43,29 @@ class RandCode(object):
 				if json_data['res_code'] == 0:
 					result = True
 		return result
+		
+	def send_sms(self, phone, randcode, exp_time='2'):
+		result = False
+		if self.ACCESS_TOKEN and self.RANDCODE_TOKEN:
+			data = {
+				'app_id':self.APP_ID,
+				'access_token':self.ACCESS_TOKEN,
+				'token':self.RANDCODE_TOKEN,
+				'phone':phone,
+				'randcode':str(randcode),
+				'exp_time':exp_time,
+				'timestamp':self.__date_time(),
+				}
+			data = self.__build_request_string(data)
+			data = self.__data_sign(data)
+			if data:
+				res = self.__request_data('post', data, self.RANDCODE_SENDSMS_API)
+				print(res)
+				json_data = json.loads(res)
+				if json_data['res_code'] == 0:
+					result = True
+		return result
+		pass
 	
 	def __request_data(self, method, data, url):
 		if isinstance(data, dict):
@@ -111,5 +135,6 @@ class RandCode(object):
 
 
 if __name__  == '__main__':
-	r = RandCode('385511200000000000', '315c4c6c0810e6c3701e5565d12e1c2b')
-	r.send('18907310000', 'http://open.189.cn/rand_code.php', '3')
+	r = RandCode('app_id', 'app_secret')
+	r.send('phone number', 'http://yourdomain/rand_code.php', '3')
+	r.send_sms('phone number', 189189)
